a33748ad752ee414b3f33fe222b53fd32b2bd122
Daniel Ioni <rossieugenio17@gmail.com>
2026-08-06 13:42:37 +0200

    fix: clean server.js - single declaration of all routes (securityRoutes fixed)

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ const rewardRoutes = require('./routes/rewards');
 const contributorsRoutes = require('./routes/contributors');
 const sensorRoutes = require('./routes/sensors');
 const securityRoutes = require('./routes/security');
+const xmrRoutes = require('./routes/xmr');
 
 // Health check
 app.get('/api/health', (req, res) => {
@@ -63,6 +64,7 @@ app.use('/api/rewards', rewardRoutes);
 app.use('/api/contributors', contributorsRoutes);
 app.use('/api/sensors', sensorRoutes);
 app.use('/api/security', securityRoutes);
+app.use('/api/xmr', xmrRoutes);
 
 // Robot routes
 try {
